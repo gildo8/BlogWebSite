0fa94d766f332bd508e5a20282a2b9537100940c
Gil <gildoron8@walla.com>
2021-11-05 20:49:35 +0200

    Added  relevant routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import Register from "./components/pages/register/Register";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 import TopBar from "./components/topbar/TopBar";
-
+import Home from "./components/pages/home/Home";
+import Register from "./components/pages/register/Register";
+import Login from "./components/pages/login/Login";
+import Write from "./components/pages/write/Write";
+import Setting from "./components/pages/settings/Settings";
+import Single from "./components/pages/single/Single";
 
 function App() {
   return (
-    <>
-    <div className="App">
-        <TopBar/>
-        <Register/>
-    </div>
-    </>
+<BrowserRouter>
+    <TopBar/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/write" element={<Write/>}/>
+      <Route path="/settings" element={<Setting/>}/>
+      <Route path="/post/:postId" element={<Single/>}/>
+    </Routes>
+</BrowserRouter>
   );
 }
 
