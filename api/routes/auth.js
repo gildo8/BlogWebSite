3582cd3540e48fa9a9b0f79a76cef08df663e6ca
Gil <gildoron8@walla.com>
2021-11-21 22:21:05 +0200

    Now users can login with userName and password.

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,41 +2,51 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require('bcrypt');
 
-router.post("/register", async(req,res) => {
-    try{
+//REGISTER
+router.post("/register", async (req, res) => {
+    try {     
+      const salt = await bcrypt.genSalt(10);
+      const hashedPass = await bcrypt.hash(req.body.password, salt);
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPass,
+      });
+  
+      const user = await newUser.save();
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+      const user = await User.findOne({ username: req.body.username });
+      !user && res.status(400).json("Wrong credentials!");
 
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword,
+      const match = await new Promise((resolve, reject) => {
+        bcrypt.compare(req.body.password, user.password, function(error, res){
+          if (error) { 
+            console.log("bed");
+            reject(error); 
+          }
+            resolve(res);
         })
-
-        const user = await newUser.save();
-        console.log(user);
-        res.status(200).json(user);
+      })
+      if(match)
+      {
         
-    }catch(err){
-        res.status(500).json(err);
+        const { password, ...others } = user._doc;
+        console.log(user._doc);
+        res.status(200).json(others);  
+      }
     }
-
-});
-
-router.post("/login", async (req,res) =>{
-    try{
-        const user = await User.findOne({username: req.body.username});
-        !user && res.status(400).json("Wrong Credentials!");
-
-        const validated = await bcrypt.compare(req.body.password, user.password);
-        !validated && res.status(400).json("Wrong Credentials!");
-        const {password, ...others} = user._doc;
-        res.status(200).json(others);
-    }catch(err){
-        res.status(500).json(err);
+    catch (err) {
+      res.status(500).json(err);
     }
-});
+  });
 
 
 module.exports = router;
