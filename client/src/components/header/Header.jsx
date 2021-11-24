30703c531963cf234e53bb94c63cd5a800ce8a9c
Gil <gildoron8@walla.com>
2021-11-24 13:26:02 +0200

    some changes in style

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,8 +4,7 @@ export default function Header() {
     return (
         <div className="header">
             <div className="headerTitles">
-                <span className="headerTitleSm">React & Node</span>
-                <span className="headerTitleLg">Blog</span>
+                <span className="headerTitleLg">Travel Blog</span>
             </div>
             <img className="headerImg" src="https://i.ibb.co/Gp4zQNt/wallpaper1920x1080.jpg" alt=""/>
         </div>
