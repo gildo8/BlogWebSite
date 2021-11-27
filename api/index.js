9cf703569f466e953f0b523426b9999eaccc0b1b
Gil <gildoron8@walla.com>
2021-11-27 20:52:35 +0200

    some changes

--- api/index.js
+++ api/index.js
@@ -47,3 +47,15 @@ app.use("/api/categories", categoryRoute);
 app.listen("5000", () => {
   console.log("Backend is running.");
 });
+
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
